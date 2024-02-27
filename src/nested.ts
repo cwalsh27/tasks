e4123aa0dbdb96047aff33eff51965a3c2b07039
Coleman <cowalsh@udel.edu>
2024-02-27 17:03:09 -0500

    finished renameQuestionbyId

--- src/nested.ts
+++ src/nested.ts
@@ -202,7 +202,11 @@ export function renameQuestionById(
     targetId: number,
     newName: string
 ): Question[] {
-    return [];
+    const newArray = questions.map(
+        (question: Question): Question =>
+            question.id === targetId ? { ...question, name: newName } : question
+    );
+    return newArray;
 }
 
 /***
