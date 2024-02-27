25932ef186c851734d3bac7386defa46faa1258f
Coleman <cowalsh@udel.edu>
2024-02-26 22:45:29 -0500

    first pass of isValid

--- src/objects.ts
+++ src/objects.ts
@@ -44,7 +44,14 @@ export function isCorrect(question: Question, answer: string): boolean {
  * be exactly one of the options.
  */
 export function isValid(question: Question, answer: string): boolean {
-    return false;
+    if (question.type === "short_answer_question") {
+        return true;
+    } else {
+        const anyValidOptions = question.options.some(
+            (option: string): boolean => option === answer
+        );
+        return anyValidOptions;
+    }
 }
 
 /**
