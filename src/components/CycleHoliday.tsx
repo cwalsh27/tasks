c2111b3d10e6d31b478cc5f47739d2d15660e839
Coleman <cowalsh@udel.edu>
2024-03-04 22:53:05 -0500

    objectively functional holiday component, lacking emojis

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -26,5 +26,22 @@ const alphaTransitions: Record<Holiday, Holiday> = {
 
 export function CycleHoliday(): JSX.Element {
     const [holiday, setHoliday] = useState<Holiday>("All Saint's Day");
-    return <div>Cycle Holiday</div>;
+
+    function cycleAlpha(): void {
+        const newHoliday = alphaTransitions[holiday];
+        setHoliday(newHoliday);
+    }
+
+    function cycleYear(): void {
+        const newHoliday = yearTransitions[holiday];
+        setHoliday(newHoliday);
+    }
+
+    return (
+        <div>
+            <div>Holiday: {holiday}</div>
+            <Button onClick={cycleAlpha}>Cycle by Alphabet</Button>
+            <Button onClick={cycleYear}>Cycle by Year</Button>
+        </div>
+    );
 }
