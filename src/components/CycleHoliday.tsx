6f23bb1c39185844108017ae238f23afcc15587c
Coleman Walsh <146851951+cwalsh27@users.noreply.github.com>
2024-03-04 23:46:05 -0500

    manual Update CycleHoliday.tsx

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,6 +1,42 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
+type Holiday = "🎂" | "☘️" | "🧨" | "🦃" | "🎄";
+
+const yearTransitions: Record<Holiday, Holiday> = {
+    "☘️": "🧨",
+    "🧨": "🎂",
+    "🎂": "🦃",
+    "🦃": "🎄",
+    "🎄": "☘️"
+};
+
+const alphaTransitions: Record<Holiday, Holiday> = {
+    "🎂": "🎄",
+    "🎄": "🧨",
+    "🧨": "☘️",
+    "☘️": "🦃",
+    "🦃": "🎂"
+};
+
 export function CycleHoliday(): JSX.Element {
-    return <div>Cycle Holiday</div>;
+    const [holiday, setHoliday] = useState<Holiday>("🎂");
+
+    function cycleAlpha(): void {
+        const newHoliday = alphaTransitions[holiday];
+        setHoliday(newHoliday);
+    }
+
+    function cycleYear(): void {
+        const newHoliday = yearTransitions[holiday];
+        setHoliday(newHoliday);
+    }
+
+    return (
+        <div>
+            <div>Holiday: {holiday}</div>
+            <Button onClick={cycleAlpha}>Cycle by Alphabet</Button>
+            <Button onClick={cycleYear}>Cycle by Year</Button>
+        </div>
+    );
 }
