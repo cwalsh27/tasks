f36092b06113d8555cd100f38315cc180b0d565b
Coleman <cowalsh@udel.edu>
2024-03-04 21:22:13 -0500

    functional start attempt system

--- src/components/StartAttempt.tsx
+++ src/components/StartAttempt.tsx
@@ -2,5 +2,36 @@ import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
 export function StartAttempt(): JSX.Element {
-    return <div>Start Attempt</div>;
+    const [attemptNum, setAttemptNum] = useState<number>(4);
+    const [inProgress, setInProgress] = useState<boolean>(false);
+
+    function startQuiz(): void {
+        setAttemptNum(attemptNum - 1);
+        setInProgress(!inProgress);
+    }
+
+    function stopQuiz(): void {
+        setInProgress(!inProgress);
+    }
+
+    function addredo(): void {
+        setAttemptNum(attemptNum + 1);
+    }
+
+    return (
+        <span>
+            <Button
+                onClick={startQuiz}
+                disabled={inProgress || attemptNum === 0}
+            >
+                Start Quiz
+            </Button>
+            <Button onClick={stopQuiz} disabled={!inProgress}>
+                Stop Quiz
+            </Button>
+            <Button onClick={addredo} disabled={inProgress}>
+                Mulligan
+            </Button>
+        </span>
+    );
 }
