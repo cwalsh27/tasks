abb6f14f33e671db4d0f436b2bff39da598573c3
Coleman <cowalsh@udel.edu>
2024-03-04 23:00:26 -0500

    added emojis

--- src/components/CycleHoliday.tsx
+++ src/components/CycleHoliday.tsx
@@ -1,31 +1,26 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 
-type Holiday =
-    | "All Saint's Day"
-    | "Saint Patrick's Day"
-    | "Fourth of July"
-    | "Thanksgiving"
-    | "Christmas";
+type Holiday = "🎂" | "☘️" | "🧨" | "🦃" | "🎄";
 
 const yearTransitions: Record<Holiday, Holiday> = {
-    "Saint Patrick's Day": "Fourth of July",
-    "Fourth of July": "All Saint's Day",
-    "All Saint's Day": "Thanksgiving",
-    Thanksgiving: "Christmas",
-    Christmas: "Saint Patrick's Day"
+    "☘️": "🧨",
+    "🧨": "🎂",
+    "🎂": "🦃",
+    "🦃": "🎄",
+    "🎄": "☘️"
 };
 
 const alphaTransitions: Record<Holiday, Holiday> = {
-    "All Saint's Day": "Christmas",
-    Christmas: "Fourth of July",
-    "Fourth of July": "Saint Patrick's Day",
-    "Saint Patrick's Day": "Thanksgiving",
-    Thanksgiving: "All Saint's Day"
+    "🎂": "🎄",
+    "🎄": "🧨",
+    "🧨": "☘️",
+    "☘️": "🦃",
+    "🦃": "🎂"
 };
 
 export function CycleHoliday(): JSX.Element {
-    const [holiday, setHoliday] = useState<Holiday>("All Saint's Day");
+    const [holiday, setHoliday] = useState<Holiday>("🎂");
 
     function cycleAlpha(): void {
         const newHoliday = alphaTransitions[holiday];
