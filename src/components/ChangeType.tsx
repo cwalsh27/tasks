ebe120686a340424d3daf0ea30cddec9f5efbf98
Coleman <cowalsh@udel.edu>
2024-03-04 20:14:13 -0500

    functional change type

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,5 +3,19 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    return <div>Change Type</div>;
+    const [qtype, switchType] = useState<QuestionType>("short_answer_question");
+
+    function typeSwitch(): void {
+        if (qtype === "multiple_choice_question") {
+            switchType("short_answer_question");
+        } else {
+            switchType("multiple_choice_question");
+        }
+    }
+
+    return (
+        <span>
+            <Button onClick={typeSwitch}>Change Type</Button>
+        </span>
+    );
 }
