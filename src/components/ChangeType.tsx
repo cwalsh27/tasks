bba8ad36a801d51d2351da07dcb2345977e0521f
Coleman <cowalsh@udel.edu>
2024-03-05 21:59:17 -0500

    reverted change type

--- src/components/ChangeType.tsx
+++ src/components/ChangeType.tsx
@@ -3,19 +3,5 @@ import { Button } from "react-bootstrap";
 import { QuestionType } from "../interfaces/question";
 
 export function ChangeType(): JSX.Element {
-    const [qtype, switchType] = useState<QuestionType>("short_answer_question");
-
-    function typeSwitch(): void {
-        if (qtype === "multiple_choice_question") {
-            switchType("short_answer_question");
-        } else {
-            switchType("multiple_choice_question");
-        }
-    }
-
-    return (
-        <span>
-            <Button onClick={typeSwitch}>Change Type</Button>
-        </span>
-    );
+    return <div>Cycle Holiday</div>;
 }
